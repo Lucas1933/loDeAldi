50114f4eb161d163d51c41a2f2d7b18019d0adc0
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-02-16 14:03:11 -0300

    add displayNavBar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 /* import "tw-elements"; */
-import { displayHeader } from "./display.js";
+import { displayHeader, displayNavBar } from "./display.js";
 import { routerInit } from "./route.js";
 document.getElementById("index").appendChild(document.createElement("main"));
 displayHeader();
+displayNavBar();
 routerInit();
