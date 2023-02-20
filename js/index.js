6dada221c2f9514ce81d2efe50a2d5d0e2d1f0eb
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-02-20 16:37:37 -0300

    edit index.js, ready to merge

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 /* import "tw-elements"; */
-import { routerInit, urlRoute } from "./router.js";
-let main = document.createElement("main");
-let button = document.createElement("button");
-let index = document.getElementById("index");
-index.appendChild(main);
-/* button.innerHTML = "rotiseria";
-button.id = "rotiseria";  */
-
-/* displayHeader(); */
-/* displayNavBar(); */
-/* main.appendChild(button);
-button.addEventListener("click", (event) => {
-  urlRoute(event);
-}); */
+import { routerInit } from "./router.js";
+import { RENDER } from "./render.js";
+RENDER.root.appendChild(RENDER.home);
 routerInit();
