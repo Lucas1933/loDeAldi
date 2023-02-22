6e8a7a7d101ff4c9c162d37ddd413e59558c432f
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-02-22 19:04:38 -0300

    refactor button to sections to a switch

--- js/render.js
+++ js/render.js
@@ -16,14 +16,134 @@ export const RENDER = {
   currentHeader: () => {
     return document.getElementById("index").getElementsByTagName("header")[0];
   },
-  setHeaderListeners: setHeaderListeners,
-  setNavBtnsListeners: setNavBtnsListeners,
+  setHeaderListeners: () => {
+    /* function changeBgColors(event, rotiseriaIcon, pasteleriaIcon) {
+      let iconID = event.target.id;
+      if (iconID == "/rotiseria") {
+        if (!rotiseriaIcon.classList.contains("bg-secondary-R")) {
+          rotiseriaIcon.classList.add("bg-secondary-R");
+          pasteleriaIcon.classList.remove("bg-secondary-P");
+        }
+      } else {
+        if (!pasteleriaIcon.classList.contains("bg-secondary-P")) {
+          pasteleriaIcon.classList.add("bg-secondary-P");
+          rotiseriaIcon.classList.remove("bg-secondary-R");
+        }
+      }
+    } */
+    function setListeners() {
+      let rotiseriaIcon = document.getElementById("/pasteleria");
+      let pasteleriaIcon = document.getElementById("/rotiseria");
+      let logo = document.getElementById("/");
+      let switchContainer = document.getElementById("switchContainer");
+      let switchElement = switchContainer.firstElementChild;
+      logo.addEventListener("click", (event) => {
+        urlRoute(event);
+      });
+      if (window.location.pathname.includes("/rotiseria")) {
+        document
+          .getElementsByClassName("slideSwitchR")[0]
+          .classList.remove("hidden");
+        document
+          .getElementsByClassName("slideSwitchP")[0]
+          .classList.add("hidden");
+      } else {
+        document
+          .getElementsByClassName("slideSwitchR")[0]
+          .classList.add("hidden");
+        document
+          .getElementsByClassName("slideSwitchP")[0]
+          .classList.remove("hidden");
+      }
+      rotiseriaIcon.addEventListener("click", (event) => {
+        urlRoute(event);
+        document
+          .getElementsByClassName("slideSwitchP")[0]
+          .classList.remove("hidden");
+        document
+          .getElementsByClassName("slideSwitchR")[0]
+          .classList.add("hidden");
+      });
+      pasteleriaIcon.addEventListener("click", (event) => {
+        urlRoute(event);
+        document
+          .getElementsByClassName("slideSwitchR")[0]
+          .classList.remove("hidden");
+        document
+          .getElementsByClassName("slideSwitchP")[0]
+          .classList.add("hidden");
+      });
+
+      /* rotiseriaIcon.addEventListener("click", (event) => {
+        urlRoute(event); */
+      /* changeBgColors(event, rotiseriaIcon, pasteleriaIcon); */
+      /*  if (!rotiseriaIcon.classList.contains("bg-secondary-R")) {
+          rotiseriaIcon.classList.add("bg-secondary-R");
+          pasteleriaIcon.classList.remove("bg-secondary-P");
+        } */
+      /*  }); */
+      /* pasteleriaIcon.addEventListener("click", (event) => {
+        urlRoute(event); */
+      /*    changeBgColors(event, rotiseriaIcon, pasteleriaIcon); */
+      /*     if (!pasteleriaIcon.classList.contains("bg-secondary-P")) {
+          pasteleriaIcon.classList.add("bg-secondary-P");
+          rotiseriaIcon.classList.remove("bg-secondary-R");
+        } */
+      /* }); */
+    }
+    setListeners();
+  },
+  setNavBtnsListeners: () => {
+    function setNavBtnsBgColor(btnTriggerEvent, btnsDivs) {
+      btnsDivs.forEach((cadaBtnDiv) => {
+        cadaBtnDiv.classList.remove("bg-secondary-R");
+        cadaBtnDiv.firstElementChild.firstElementChild.classList.remove(
+          "animate-bounce"
+        );
+      });
+      btnTriggerEvent.target.parentNode.parentNode.classList.add(
+        "bg-secondary-R"
+      );
+      btnTriggerEvent.target.parentNode.parentNode.firstElementChild.firstElementChild.classList.add(
+        "animate-bounce"
+      );
+    }
+    function setNavBtnsListeners() {
+      let btnsDivs = Array.from(document.getElementsByClassName("navBarIcons"));
+      let btns = [];
+      btnsDivs.forEach((cadaDiv) => {
+        btns.push(cadaDiv.firstElementChild);
+      });
+
+      btns.forEach((cadaBtn) => {
+        cadaBtn.addEventListener("click", (btnTriggerEvent) => {
+          setNavBtnsBgColor(btnTriggerEvent, btnsDivs);
+          urlRoute(btnTriggerEvent);
+        });
+      });
+    }
+    setNavBtnsListeners();
+  },
 };
 
-function getError404() {
-  let main = document.createElement("main");
-  main.innerHTML = "404 not found";
-  return main;
+function getHome() {
+  let home = document.createElement("main");
+  home.classList.add("home");
+  home.innerHTML = `
+    <button
+      id="/rotiseria"
+      class="mx-4 mb-4 border-2 border-solid border-black bg-black font-semibold text-white"
+    >
+      Rotiseria
+    </button>
+    <button
+      id="/pasteleria"
+      class="mx-4 border-2 border-solid border-pink-700 bg-pink-500 font-semibold text-white"
+    >
+      Pasteleria
+    </button>
+  `;
+  return home;
 }
 function getRotiseriaHome() {
   let rotiseriaHome = document.createElement("main");
@@ -38,20 +158,6 @@ function getPasteleriaHome() {
   pasteleriaHome.classList.add("pasteleriaHome");
   pasteleriaHome.innerHTML = `Las cosas dulces mas ricas!`;
   return pasteleriaHome;
-}
-function setNavBtnsListeners() {
-  let btnsDivs = Array.from(document.getElementsByClassName("navBarIcons"));
-  let btns = [];
-  btnsDivs.forEach((cadaDiv) => {
-    btns.push(cadaDiv.firstElementChild);
-  });
-
-  btns.forEach((cadaBtn) => {
-    cadaBtn.addEventListener("click", (btnTriggerEvent) => {
-      setNavBtnsBgColor(btnTriggerEvent, btnsDivs);
-      urlRoute(btnTriggerEvent);
-    });
-  });
 }
 function getNavBar() {
   let navBar = document.createElement("nav");
@@ -91,40 +197,9 @@ function getNavBar() {
     `;
   return navBar;
 }
-function setNavBtnsBgColor(btnTriggerEvent, btnsDivs) {
-  btnsDivs.forEach((cadaBtnDiv) => {
-    cadaBtnDiv.classList.remove("bg-secondary-R");
-    cadaBtnDiv.firstElementChild.firstElementChild.classList.remove(
-      "animate-bounce"
-    );
-  });
-  btnTriggerEvent.target.parentNode.parentNode.classList.add("bg-secondary-R");
-  btnTriggerEvent.target.parentNode.parentNode.firstElementChild.firstElementChild.classList.add(
-    "animate-bounce"
-  );
-}
-function getHome() {
-  let home = document.createElement("main");
-  home.classList.add("home");
-  home.innerHTML = `
-    <button
-      id="/rotiseria"
-      class="mx-4 mb-4 border-2 border-solid border-black bg-black font-semibold text-white"
-    >
-      Rotiseria
-    </button>
-    <button
-      id="/pasteleria"
-      class="mx-4 border-2 border-solid border-pink-700 bg-pink-500 font-semibold text-white"
-    >
-      Pasteleria
-    </button>
-  `;
-  return home;
-}
 function getHeader() {
   let header = document.createElement("header");
-  header.innerHTML = `  <div
+  /* header.innerHTML = `  <div
   class=""
 >
   <div class="flex items-center justify-center">
@@ -151,29 +226,56 @@ function getHeader() {
       alt=""
     />
   </div>
+</div>`; */
+  header.innerHTML = `<div class="headerRotiseria">
+  <div class="flex items-center justify-center">
+    <img
+      id="/"
+      class="w-40"
+      src="./assets/icons/header/logo.png"
+      alt=""
+    />
+  </div>
+  <div class="flex grow justify-center" id="switchContainer">
+      <div
+    class="slideSwitchR"
+    
+  >
+    <div
+      class="flex w-12 justify-center rounded-full border-2 border-solid"
+    >
+      <img
+      id="/pasteleria"
+        class="w-8"
+        src="./assets/icons/header/rotiseria.svg"
+        alt=""
+      />
+    </div>
+    <p class="block text-white">Rotiseria</p>
+  </div>
+    <div
+      class="slideSwitchP"
+      
+    >
+      <div
+        class="flex w-12 justify-center rounded-full border-2 border-solid border-pink-900"
+      >
+        <img
+         id="/rotiseria"
+          class="w-8"
+          src="./assets/icons/header/pasteleria.svg"
+          alt=""
+        />
+      </div>
+
+      <p class="text-pink-200">Pasteleria</p>
+    </div>
+  </div>
 </div>`;
   return header;
 }
-
-function setHeaderListeners() {
-  let rotiseriaIcon = document.getElementById("/rotiseria");
-  let pasteleriaIcon = document.getElementById("/pasteleria");
-  let logo = document.getElementById("/");
-  logo.addEventListener("click", (event) => {
-    urlRoute(event);
-  });
-  rotiseriaIcon.addEventListener("click", (event) => {
-    urlRoute(event);
-    if (!rotiseriaIcon.classList.contains("bg-secondary-R")) {
-      rotiseriaIcon.classList.add("bg-secondary-R");
-      pasteleriaIcon.classList.remove("bg-secondary-P");
-    }
-  });
-  pasteleriaIcon.addEventListener("click", (event) => {
-    urlRoute(event);
-    if (!pasteleriaIcon.classList.contains("bg-secondary-P")) {
-      pasteleriaIcon.classList.add("bg-secondary-P");
-      rotiseriaIcon.classList.remove("bg-secondary-R");
-    }
-  });
+function getError404() {
+  let main = document.createElement("main");
+  main.innerHTML = "404 not found";
+  return main;
 }
