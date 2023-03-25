c0917d29cf41fada671da72492c8be421d196114
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2023-03-25 19:08:12 -0300

    add bigger navBar rotiseria icons, add more light colors, add bigger text to extras

--- js/menu.js
+++ js/menu.js
@@ -195,7 +195,7 @@ function getBurguerExtras() {
   );
   aside.innerHTML = `<div class="mb-4">
       <button
-        class="text-md inline-block h-32 w-10 rounded-t-lg rounded-b-lg border-2 border-solid border-black bg-red-700 font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg"
+        class="text-xl inline-block h-40 w-10 rounded-t-lg rounded-b-lg border-2 border-solid border-black bg-red-700 font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#collapseWidthExample"
@@ -210,11 +210,11 @@ function getBurguerExtras() {
         s <br />
       </button>
     </div>
-    <div style="min-height: 120px">
-      <div class="collapse-horizontal collapse" id="collapseWidthExample">
+    <div class="flex items-center min-h-[125px]" >
+      <div class="collapse-horizontal collapse max-w-sm" id="collapseWidthExample">
         <div
-          class="block max-w-sm rounded-lg bg-white p-6 shadow-lg"
-          style="width: 300px"
+          class="block text-xl  rounded-lg bg-white p-6 shadow-lg"
+          
         >
           Otro medallon = $300 <br />
           Papas con cheddar, panceta y verdeo = $250
